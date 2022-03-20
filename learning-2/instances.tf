a03c62fead58fdb72e6b75086468218fe1f68360
vibakar <vibakar23@gmail.com>
2022-03-20 08:57:18 +0000

    learning-2: Moved resources to seperate files, used variables, for and foreach

--- learning-2/instances.tf
+++ learning-2/instances.tf
@@ -1,26 +1,6 @@
 resource "aws_instance" "example" {
-  ami           = "ami-03e88be9ecff64781"
-  instance_type = "t2.micro"
-}
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = var.INSTANCE_TYPE
 
-resource "aws_default_vpc" "example" {
-}
-
-resource "aws_default_security_group" "example" {
-  vpc_id = aws_default_vpc.example.id
-
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  tags = {for k, v in var.TAGS : k => lower(v)}
 }
